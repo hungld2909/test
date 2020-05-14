1e7153573539b4e572a82835a25833b44e3ba620
Hung LD <hungldgch29091996@gmail.com>
2020-05-14 14:48:33 +0700

    Add hello.dart

---
+++ lib/hello.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Hello extends StatefulWidget {
+
+  @override
+  _HelloState createState() => _HelloState();
+}
+
+class _HelloState extends State<Hello> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(g
+       child: Text("Hiu hiu"),
+    );
+  }
+}
